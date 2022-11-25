340c0c2620c35b926a30ce3caa67f603b50b9f3b
nrjkumar <neerajk764@gmail.com>
2022-11-25 18:55:02 -0500

    Added-plots

--- Visualizations/plots.py
+++ Visualizations/plots.py
@@ -1,4 +1,4 @@
-import os.path
+import os.path, sys
 import seaborn  as sns
 import pandas as pd
 import numpy as np
@@ -6,18 +6,198 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
 TIDY_DATA_DIR = os.path.expanduser('~')+'/nhlapidata/csv/tidy_data.csv'
-SAVE_PLOT_DIR = os.path.join(os.path.dirname(__file__), '..', '..', 'figures')
+#SAVE_PLOT_DIR = os.path.join(os.path.dirname(__file__), 'figures')
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+SAVE_PLOT_DIR = parentdir + '/figures/'
+#print(TIDY_DATA_DIR)
+print(SAVE_PLOT_DIR)
 
 class IceHockey_plot:
     def __init__(self, fig_size=(15, 10)):
         self.fig_size = fig_size
 
-    def Goalrate_distance_and_angle(self, df:pd.DataFrame, save_figure= True):
+    def milestone2_goal_rate_vs_distance_and_angle(self, df: pd.DataFrame, save_fig=True) -> plt.Figure:
+        """
+        Plots a comparative graph across seasons (2017 - 2020) of the relationship between
+        shot distance and goals (as described in Part 5 Q2)
+        :param df: tidy pandas.DataFrame
+        :param save_fig: boolean to save the plot to SAVE_PLOT_DIR
+        :return: a plt.Figure object instance
+        """
 
-        """Plots a comparison graph across seasons for relationship between shot distance and goals"""
-        
         fig = plt.figure(figsize=(25, 20))
 
         plt.subplot(221)
         filtered_df = df[df['shot_distance'].notnull()]
         filtered_df['shot_distance'] = filtered_df['shot_distance'].round(0)
+        filtered_df = filtered_df.groupby(["shot_distance"])["is_goal"].mean().to_frame().reset_index()
+        ax = sns.lineplot(x='shot_distance', y='is_goal', data=filtered_df)
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xticks(np.arange(0, 210, 10), rotation=20)
+        plt.yticks(np.arange(0, 1.2, 0.2))
+        plt.xlabel('Shot distance (ft)')
+        plt.ylabel('Goal Probability')
+        plt.title('Goal Probability vs Distance')
+
+        plt.subplot(222)
+        ax = sns.histplot(data=df, x='shot_distance', hue='is_goal', multiple='stack')
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xticks(np.arange(0, 210, 10), rotation=20)
+        plt.xlabel('Shot distance (ft)')
+        plt.ylabel('Count')
+        plt.title('Histogram of shot distance')
+
+        plt.subplot(223)
+        filtered_df = df[df['shot_angle'].notnull()]
+        filtered_df['shot_angle'] = filtered_df['shot_angle'].round(0)
+        filtered_df = filtered_df.groupby(["shot_angle"])["is_goal"].mean().to_frame().reset_index()
+        ax = sns.lineplot(x='shot_angle', y='is_goal', data=filtered_df)
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xticks(np.arange(0, 210, 10), rotation=20)
+        plt.yticks(np.arange(0, 1.2, 0.2))
+        plt.xlabel('Shot Angle (deg rel. to center-line)')
+        plt.ylabel('Goal Probability')
+        plt.title('Goal Probability vs Angle')
+
+        plt.subplot(224)
+        ax = sns.histplot(data=df, x='shot_angle', hue='is_goal', multiple='stack')
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xticks(np.arange(0, 190, 10), rotation=20)
+        plt.xlabel('Shot Angle (deg rel. to center-line)')
+        plt.ylabel('Goal Probability')
+        plt.title('Histogram of shot angle')
+
+        plt.show()
+        if save_fig:
+            fig.savefig(os.path.join(SAVE_PLOT_DIR, 'milestone2', "Q2-2-goal_rate.png"))
+        return fig
+
+    def milestone2_goal_dist_by_empty_net(self, df: pd.DataFrame, save_fig=True) -> plt.Figure:
+
+        fig = plt.figure(figsize=(20, 25))
+        filtered_df = df[df['is_goal'] == True]
+        non_empty_net = filtered_df[filtered_df['is_empty_net'] == False]
+        non_empty_net_zoomed = non_empty_net[non_empty_net['shot_distance'] >= 100]
+        empty_net = filtered_df[filtered_df['is_empty_net'] == True]
+
+        plt.subplot(311)
+        ax = sns.histplot(data=non_empty_net, x='shot_distance')
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xticks(np.arange(0, 210, 10), rotation=20)
+        plt.xlabel('Shot distance (ft)')
+        plt.ylabel('Count')
+        plt.title('Histogram of shot distance for goals (non-empty net)')
+        left, bottom, width, height = (100, -50, 100, 500)
+        rect = mpatches.Rectangle((left, bottom), width, height,
+                                  fill=False,
+                                  color="red",
+                                  linewidth=2)
+        plt.gca().add_patch(rect)
+        plt.text(130, 500, 'Zone of interest shown below', fontsize=15)
+
+        plt.subplot(312)
+        ax = sns.histplot(data=non_empty_net_zoomed, x='shot_distance')
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xticks(np.arange(100, 210, 10), rotation=20)
+        plt.xlabel('Shot distance (ft)')
+        plt.ylabel('Count')
+        plt.title(
+            'Histogram of shot distance for goals (non-empty net) \n Zoom on the 100ft to 200ft shot distance zone')
+        plt.text(130, 16.5, f'Total number of instance for all seasons: {non_empty_net_zoomed.shape[0]}', fontsize=14)
+
+        plt.subplot(313)
+        ax = sns.histplot(data=empty_net, x='shot_distance')
+        ax.set_axisbelow(True)
+        ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xticks(np.arange(0, 210, 10), rotation=20)
+        plt.xlabel('Shot distance (ft)')
+        plt.ylabel('Count')
+        plt.title('Histogram of shot distance for goals (empty net)')
+
+        plt.show()
+        if save_fig:
+            fig.savefig(os.path.join(SAVE_PLOT_DIR, 'milestone2', "Q2-3-empty_net_goal_dist.png"))
+        return fig
+
+    
+    def milestone2_shot_dist_by_is_goal(self, df: pd.DataFrame, save_fig=True):
+        fig = plt.figure(figsize=(10, 10))
+
+        ax = sns.histplot(data=df, x="shot_distance", hue="is_goal", multiple ="stack", bins=20)
+        #ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xlabel('Shot distance (ft)')
+        plt.title('Histogram of shot distance for shots (goals and no-goals separated)')
+        ax.legend(['Goal','No Goal'], title = "Shot Result")
+        
+        plt.show()
+        if save_fig:
+            fig.savefig(os.path.join(SAVE_PLOT_DIR, 'milestone2', "Q2-1-shot_dist_by_is_goal.png"))
+        
+        return fig
+    
+    def milestone2_shot_angle_by_is_goal(self, df: pd.DataFrame, save_fig=True):
+        fig = plt.figure(figsize=(10, 10))
+
+        ax = sns.histplot(data=df, x="shot_distance", hue="is_goal", multiple ="stack", bins=20)
+        #ax.yaxis.grid(color='gray', linestyle='dashed')
+        plt.xlabel('Shot distance (ft)')
+        plt.title('Histogram of shot distance for shots (goals and no-goals separated)')
+        ax.legend(['Goal','No Goal'], title = "Shot Result")
+        
+        plt.show()
+        if save_fig:
+            fig.savefig(os.path.join(SAVE_PLOT_DIR, 'milestone2', "Q2-1-shot_angle_by_is_goal.png"))        
+        return fig
+    
+    def milestone2_2Dhist_shot_angle_distance(self, df: pd.DataFrame, save_fig=True):
+        
+        fig = plt.figure(figsize=(20, 20))
+
+        ax = sns.jointplot(data=df, x="shot_distance", y="shot_angle", kind="hist", ratio=4)
+        ax.fig.suptitle('2D Histogram of shots by angle and distance from net')
+        ax.fig.tight_layout()
+        ax.ax_joint.set_xlabel("Shot Distance (feet)")
+        ax.ax_joint.set_ylabel("Shot Angle (Degree)")
+               
+        plt.show()
+        if save_fig:
+            ax.savefig(os.path.join(SAVE_PLOT_DIR, 'milestone2', "Q2-1-2Dhist_shot_angle_distance.png"))  
+        return fig    
+
+def main():
+    df = pd.read_csv(TIDY_DATA_DIR)
+
+    hockey_plotter = IceHockey_plot()
+
+    # ---- Milestone 1 Plots ----
+    # # Plot Q5.1
+    # hockey_plotter.milestone1_shot_type_histogram(df)
+    # # Plot Q5.2
+    # hockey_plotter.milestone1_distance_vs_goal_chance(df)
+    # # Plot Q5.3
+    # hockey_plotter.milestone1_distance_and_type_vs_goal(df)
+
+    # ---- Milestone 2 Plots ----
+
+    # Plot Q2.1
+    hockey_plotter.milestone2_shot_dist_by_is_goal(df)
+    hockey_plotter.milestone2_shot_angle_by_is_goal(df)
+    hockey_plotter.milestone2_2Dhist_shot_angle_distance(df)
+    
+    # Plot Q2.2
+    hockey_plotter.milestone2_goal_rate_vs_distance_and_angle(df)
+
+    # Plot Q2.3
+    hockey_plotter.milestone2_goal_dist_by_empty_net(df)
+
+if __name__ == "__main__":
+    main()
---
+++ models/xgboost/xgb_5_3.py
@@ -0,0 +1,201 @@
+from comet_ml import Experiment
+import shap,sys,os
+import pandas as pd
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import chi2
+from xgboost import XGBClassifier
+# import figure plot
+from sklearn.model_selection import cross_val_score
+import pickle
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+print(currentdir)
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+#utils_path = os.path.abspath(os.path.join('..'))
+from create_plot import *
+from utils import *
+#from ift6758.models.utils import *
+from dotenv import load_dotenv
+load_dotenv()
+COMET_API_KEY = os.getenv('COMET_API_KEY') 
+
+def shap_feature(X, y, model):
+
+    xgb_model = model.fit(X, y)
+
+    # explain the model's predictions using SHAP
+    # (same syntax works for LightGBM, CatBoost, scikit-learn, transformers, Spark, etc.)
+    explainer = shap.Explainer(xgb_model)
+    shap_values = explainer(X)
+    
+    # visualize the first prediction's explanation
+    shap.plots.waterfall(shap_values[0])
+    plt.close()
+    return None
+
+
+def feature_selection(df_X, df_y, model):
+
+    # Normalization 
+    # Zscore and Min-Max normalization
+    cols = ['x_coordinate', 'y_coordinate',
+               'game_time(s)', 'prev_event_x', 'prev_event_y',
+               'time_since_prev_event', 'distance_to_prev_event',
+               'speed_since_prev_event', 'shot_distance', 'shot_angle',
+               'change_in_angle', 'time_since_pp'
+               ]
+    
+    df_zscore = df_X.copy()
+    df_minmax = df_X.copy()
+    
+    for col in cols:
+        # Z score normalization
+        df_zscore[col] = (df_X[col] - df_X[col].mean())/df_X[col].std(ddof=0)
+        # min max normalization
+        df_minmax[col] = (df_X[col] - df_X[col].min())/ (df_X[col].max()- df_X[col].min())
+    
+    #without nomarlization
+    X = df_X.to_numpy()
+    y = df_y.to_numpy()
+    base_scores_mean = np.mean(cross_val_score(model, X, y, cv=5))
+    
+    # Z-score normalization
+    X = df_zscore.to_numpy()
+    y = df_y.to_numpy()
+    z_score_scores_mean = np.mean(cross_val_score(model, X, y, cv=5))
+
+    # min-max normalization
+    X = df_minmax.to_numpy()
+    y = df_y.to_numpy()
+    minmax_scores_mean = np.mean(cross_val_score(model, X, y, cv=5))
+
+    #Removing features with low variance
+    X = df_X.to_numpy()
+    y = df_y.to_numpy()
+    sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
+    X_new = sel.fit_transform(X)
+    lowvar_scores_mean = np.mean(cross_val_score(model, X_new, y, cv=5))
+    
+    #Univariate feature selection
+    database = df_minmax.to_numpy()
+    X = df_minmax.to_numpy()
+    y = df_y.to_numpy()
+    X_new = SelectKBest(chi2, k=10).fit_transform(X, y)
+    univar_scores_mean = np.mean(cross_val_score(model, X_new, y, cv=5))
+
+
+    # forward search
+    X = df_X.to_numpy()
+    y = df_y.to_numpy()
+    # start with shot_distance because it is the best relation as showing by shap figure
+    X_new = X[:,11:12]
+    
+    scores_base = np.mean(cross_val_score(model, X_new, y, cv=5))
+    index = [11]
+    for i in range(X.shape[1]):
+        print('hi')
+        if (i == 11):
+            pass
+        else:
+            X_add = np.concatenate((X_new, X[:,i].reshape(-1,1)), axis=1)
+            scores = np.mean(cross_val_score(model, X_add, y, cv=5))
+            if (scores >= scores_base):
+                X_new = X_add
+                scores_base = scores
+                index.append(i)
+    forward_search_scores_mean = scores_base
+    forward_search_idx = index
+
+    result = {"base_scores_mean  "  :base_scores_mean,
+              "z_score_scores_mean"  :z_score_scores_mean,
+              "minmax_scores_mean"  :minmax_scores_mean,
+              "lowvar_scores_mean"  :lowvar_scores_mean,
+              "univar_scores_mean"  :univar_scores_mean,
+              "forward_search_scores_mean"  :forward_search_scores_mean,
+              "forward_search_idx"  :forward_search_idx}
+    return result
+
+def main():
+    '''
+    use the best hyperparameter and combine with feature selection methods:
+    z-score normalization
+    minmax normalization
+    remove features with low variance
+    Univariate feature selection
+    forward search
+    save features selected to comet
+    '''
+    feature = ['period', 'x_coordinate', 'y_coordinate',
+               'game_time(s)', 'prev_event_x', 'prev_event_y',
+               'time_since_prev_event', 'is_rebound', 'distance_to_prev_event',
+               'speed_since_prev_event', 'shot_distance', 'shot_angle',
+               'change_in_angle', 'shot_type', 'prev_event_type', 'time_since_pp',
+               'home_strength', 'away_strength']
+    
+    df_X, df_y, _, _ = load_data(
+        features=feature,
+        train_val_seasons=DEFAULT_TRAIN_SEASONS,
+        test_season=DEFAULT_TEST_SEASONS,
+        do_split_val=False,
+        target='is_goal',
+        use_standard_scaler=False,
+        drop_all_na=True,
+        convert_bool_to_int=True,
+        one_hot_encode_categoricals=True
+    )
+    
+    params = {'objective':'binary:logistic',
+              'base_score': 0.5,
+              'colsample_bylevel': 1,
+              'colsample_bynode': 1,
+              'colsample_bytree': 1,
+              'gamma': 1.0644957574704856e-06,
+              'gpu_id': -1,
+              'importance_type':'gain',
+              'interaction_constraints':'',
+              'learning_rate': 0.253149986,
+              'max_delta_step': 0,
+              'max_depth': 8,
+              'min_child_weight': 1,
+              'monotone_constraints':'()',
+              'n_estimators': 709,
+              'n_jobs': 4,
+              'num_parallel_tree': 1,
+              'random_state': 0,
+              'reg_alpha': 1.86814141e-05,
+              'reg_lambda': 1.74496799e-05,
+              'scale_pos_weight': 4,
+              'subsample': 1,
+              'tree_method':'exact',
+              'validate_parameters': 1,
+              'verbosity': None,
+              'alpha': 1.8681414898850136e-05,
+              'eta': 0.2531499873091687,
+              'grow_policy':'depthwise',
+              'lambda': 1.744968010669035e-05,
+              'eval_metric'      :  'error'
+            }
+    
+    xgb_model = XGBClassifier(**params)
+      
+    result = feature_selection(df_X, df_y, xgb_model)
+    
+    shap_feature(df_X, df_y, xgb_model)
+    
+    file_name = "xgb_feature.pkl"
+    pickle.dump(xgb_model, open(file_name, "wb"))
+    
+    experiment = Experiment(
+        api_key=COMET_API_KEY,
+        project_name="milestone2",
+        workspace="ift6758-17",
+    )
+    experiment.log_metrics(result)
+    experiment.log_model('5_3_best_features', 'xgb_features.pkl')
+    print('result is = ',result)
+if __name__ == "__main__":
+    main()
